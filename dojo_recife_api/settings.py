e71dd9ecdd4b7e40fd674dc6661bf741adba3353
Juniooor <juniormodern@hotmail.com>
2023-04-02 23:33:14 -0300

    apenas configurando ambiente

--- dojo_recife_api/settings.py
+++ dojo_recife_api/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-from decouple import config, Csv
+
+from decouple import Csv, config
 from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
