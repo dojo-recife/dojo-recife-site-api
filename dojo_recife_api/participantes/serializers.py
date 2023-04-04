147e35721552005145444af4380ab35dd3eb9a18
Wallys Ferreira <wallysfsilva@gmail.com>
2023-04-04 16:23:56 -0300

    adicionar validacao de datetime limite de inscricao do evento

--- dojo_recife_api/participantes/serializers.py
+++ dojo_recife_api/participantes/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from .models import Participante
+from django.utils import timezone
 
 
 class ParticipanteSerializer(serializers.ModelSerializer):
+    def validate_evento(self, value):
+        if value.data_hora_limite < timezone.now():
+            raise serializers.ValidationError('Passou do horário de inscrição do evento')
+
+        return value
+
     class Meta:
         model = Participante
         fields = ("id", "nome", "email", "telefone", "documento", "evento")
